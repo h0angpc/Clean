3f92c5463a5e203cd207efffaebd09cd9b9d2412
788465h10 <dolongnhat0301@gmail.com>
2024-10-07 14:58:12 +0700

    refactor: finish bookingstep3 page and add two Arrow svg for this page

---
+++ src/app/(booking)/(routes)/booking/step-3/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React from 'react'
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+
+const Step_3 = () => {
+  return (
+    <div className="h-full w-full">
+      <div className="w-1/2 m-auto p-4">
+        <div className="justify-center h-max">
+          <p className="text-4xl text-center font-bold mb-2 mt-[50px] ">
+            Book Timing
+          </p>
+          <p className="text-[20px] text-center text-[#88939D] font-normal leading-[25px]">
+            Save even more by booking off-peak dates and times.
+          </p>
+        </div>
+    </div>
+    <div className="flex justify-center gap-4 my-[20px]">
+      <Image className='hover:cursor-pointer' src="/img/Left_Arrow.svg" alt="arrow-left" width={20} height={20} />
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">22</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">23</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-blue-500 border border-blue-500 hover:text-white">24</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">25</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">26</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">27</Button>
+      <Button className="bg-white h-[55px] w-[132px] rounded-[10px] font-semibold text-xl text-[#4f6071] border border-[#d3d8dd] hover:text-white">28</Button>
+      <Image className='hover:cursor-pointer' src="/img/Right_Arrow.svg" alt="arrow-left" width={20} height={20} />
+
+    </div>
+
+    <div className="grid w-3/4 m-auto justify-center gap-4 mt-[40px]">
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-xl text-blue-500 border border-blue-500 hover:text-white">
+        <div className="grid grid-cols-2">
+            <p className="text-base font-semibold leading-[23px] tracking-tight text-left">Flexible</p>
+            <p className="text-xs font-semibold leading-[14px] tracking-tight text-right self-center">Save $8.10 off</p>
+            <p className="text-sm font-normal leading-[19px] tracking-tight text-left">Cleaner will arrive between 9am-4pm</p>
+        </div>
+      </Button>
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-base leading-[23px] tracking-tight text-[#4f6071] border border-[#d3d8dd] hover:text-white">08:00am</Button>
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-base leading-[23px] tracking-tight text-[#4f6071] border border-[#d3d8dd] hover:text-white">08:30am</Button>
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-base leading-[23px] tracking-tight text-blue-500 border border-blue-500 hover:text-white">09:00am</Button>
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-base leading-[23px] tracking-tight text-[#4f6071] border border-[#d3d8dd] hover:text-white">09:30am</Button>
+      <Button className="bg-white h-[73px] w-[400px] rounded-[10px] font-semibold text-base leading-[23px] tracking-tight text-[#4f6071] border border-[#d3d8dd] hover:text-white">10:00am</Button>
+    </div>
+    
+    <div className="flex justify-center items-center mt-[35px]">
+          <Button className="w-[165px] h-[55px] bg-[#1A78F2] text-[16px]">Next</Button>
+    </div>
+</div>
+
+  )
+}
+
+export default Step_3
