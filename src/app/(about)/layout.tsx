f26cbfe1ad06f6aae09d176573cab1942a2fad5b
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-10-11 14:48:35 +0700

    feat: add about us page

---
+++ src/app/(about)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutLayout = (props : {
+    children: React.ReactNode
+}) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default AboutLayout
