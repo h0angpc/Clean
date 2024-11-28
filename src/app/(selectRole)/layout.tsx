15cd5075111fd7a48126789662d0dcc8b858c306
788465h10 <dolongnhat0301@gmail.com>
2024-11-28 23:06:23 +0700

    feat: select role ui

---
+++ src/app/(selectRole)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SelectRoleLayout = (props: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
+export default SelectRoleLayout
--- src/app/(selectRole)/selectRole/page.tsx
+++ src/app/(selectRole)/selectRole/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react'
 
-const Select = () => {
+const SelectRole = () => {
 
   const [selectService, setSelectService] = useState('')
 
@@ -19,50 +19,42 @@ const Select = () => {
       <div className='flex flex-col w-full h-full gap-[50px]'>
         <div className='w-full h-fit'>
           <h1 className='text-[#12153a] text-[58px] font-Averta-Semibold leading-[65px] text-center'>
-            Find the Perfect Service for You!
+            Bringing Skilled Helpers and Customers Together
           </h1>
         </div>
         <div className='w-full h-fit text-center'>
           <h5 className='text-[#2b3641] text-xl font-normal font-Averta-Regular leading-[25px]'>
-            Choose the service that suits you best. 
+            Whether you’re here to offer your skills or find reliable help, we’ve got you covered. 
             <br />
-            What we have to offer:
+            Choose your path:
           </h5>
         </div>
         <div className='w-[80%] h-fit flex flex-col sm:flex-row max-sm:justify-center max-sm:items-center max-sm:divide-y-2 sm:divide-x-2 divide-dashed m-auto border-2 rounded-2xl p-[20px] shadow-xl'>
             <div 
               className='w-[80%] sm:w-[50%] h-full flex flex-col items-center justify-center gap-[10px] hover:cursor-pointer max-sm:pb-5'
-              onMouseEnter={() => setSelectService('Home Service')}
+              onMouseEnter={() => setSelectService('Helper')}
               onMouseLeave={() => setSelectService('')}
             >
-              <Image src={`${selectService !== 'Home Service' ? '/images/Select/house_unselect.png' : '/images/Select/house_select.png'}`} alt='home_service' width={200} height={200}/>
+              <Image src={`${selectService !== 'Helper' ? '/images/Select/helper_unselect.png' : '/images/Select/helper_select.png'}`} alt='helper' width={200} height={200}/>
               <div className='w-full h-[40%] flex flex-col items-center gap-[5px]'>
-                <div className='text-[#1d2c4c] text-[34px] font-Averta-Semibold leading-[42px]'>Home Cleaning</div>
+                <div className='text-[#1d2c4c] text-[34px] font-Averta-Semibold leading-[42px]'>Become a Helper</div>
                 <div className='text-[#12153a] text-base font-Averta-Regular leading-[23px] tracking-tight text-center'>
-                  Professional home cleaning services take the burden off your shoulders, 
-                  <br /> ensuring every corner shines while you focus on what matters most.
-                  <br /><span className='font-Averta-Semibold'> It’s not just cleaning; it’s peace of mind delivered.</span>
+                    Share your skills, earn on your terms, and grow your opportunities. <br />Join a platform that connects you with customers who value your expertise. <br />Enjoy flexible schedules, secure payments, and tools to help you <br />manage your work easily.
                 </div>
               </div>
             </div>
             <div 
               className='w-[80%] sm:w-[50%] h-full flex flex-col items-center justify-center gap-[10px] hover:cursor-pointer max-sm:pt-5'
-              onMouseEnter={() => setSelectService('Other Service')}
+              onMouseEnter={() => setSelectService('Customer')}
               onMouseLeave={() => setSelectService('')}  
             >
               <div className='relative h-[200px] w-[280px]'>
-                <Image src={`${selectService !== 'Other Service' ? '/images/Select/other1_unselect.png' : '/images/Select/other1_select.png'}`} alt='other_service_01' width={133.3} height={133.3} className='absolute top-0 left-0'/>
-                <Image src={`${selectService !== 'Other Service' ? '/images/Select/other2_unselect.png' : '/images/Select/other2_select.png'}`} alt='other_service_02' width={133.3} height={133.3} className='absolute bottom-0 right-0'/>
+                <Image src={`${selectService !== 'Customer' ? '/images/Select/customer_unselect.png' : '/images/Select/customer_select.png'}`} alt='customer' width={200} height={200} className='absolute bottom-0 right-0'/>
               </div>
               <div className='w-full h-[40%] flex flex-col items-center gap-[5px]'>
-                <div className='text-[#1d2c4c] text-[34px] font-Averta-Semibold leading-[42px]'>Other Services</div>
+                <div className='text-[#1d2c4c] text-[34px] font-Averta-Semibold leading-[42px]'>Become a Customer</div>
                 <div className='text-[#12153a] text-base font-Averta-Regular leading-[23px] tracking-tight text-center'>
-                  Services like babysitting, housekeeping, and caretaking are designed to 
-                  <br /> provide care, comfort, and support exactly when you need it, allowing
-                  <br /> you to focus on your priorities.
-                  <br /><span className='font-Averta-Semibold'> It’s not just assistance—it’s personalized care that makes life easier
-                    <br /> and more manageable.
-                  </span>
+                    Discover trusted helpers for every task—all in one place. From quick <br />bookings to transparent pricing, we make it simple to find skilled <br />professionals who meet your needs.
                 </div>
               </div>
             </div>
@@ -72,4 +64,4 @@ const Select = () => {
   )
 }
 
-export default Select
+export default SelectRole
